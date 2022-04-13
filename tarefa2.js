c80808333bc56e06eed5f9152a976fd6e0db1510
ilanninha <91700396+Lanaaa22@users.noreply.github.com>
2022-04-13 14:29:40 -0300

    tarefa2.js

---
+++ tarefa2.js
@@ -0,0 +1,22 @@
+const subtitulos = document.querySelectorAll("h2");
+const topicos = new Array();
+subtitulos.forEach(element => {
+	topicos.push(element.textContent);
+	
+	const variavel = document.createElement("a");
+	ancora.setAttribute("name", element.textContent);
+	element.append(variavel);
+	
+
+});
+
+const lista = document.querySelector("ol");
+topicos.forEach(topico => {
+	const item = document.createElement("li");
+	const link = document.createElement("a");
+	lista.append(item);
+	item.append(link);
+	link.setAttribute("href", `#${topico}`);
+	link.textContent = topico;
+
+});
